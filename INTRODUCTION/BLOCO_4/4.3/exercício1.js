e2b2343ac722ac5524ee8e59c1cfc46b2563e06d
vanderson-henrique <vhbenedito@gmail.com>
2020-08-31 16:05:33 -0300

    Exercícios 1 e 2

--- INTRODUCTION/BLOCO_4/4.3/exercício1.js
+++ INTRODUCTION/BLOCO_4/4.3/exercício1.js
@@ -9,4 +9,16 @@ n = 5
 *****
 */
 
+let n = 5;
+let asterisco = "";  // crio a variável que vai armazenar o *
+for (let i = 1; i <= n; i += 1){ // loop para que possa adicionar um * de acordo com o número de n
+        asterisco += "*"; // está adicionando na variável um * cada vez que entra no loop, ou seja vai ficar com o valor de n
+    }
+    
+for (let j = 1; j <= n; j += 1){ // loop para imprimir a minha variável que já tem a linha do tamanho certo o número de vezes de acordo com o valor de n
+        console.log(asterisco);
+}    
+    
+    
+
 
--- INTRODUCTION/BLOCO_4/4.3/exercício2.js
+++ INTRODUCTION/BLOCO_4/4.3/exercício2.js
@@ -7,3 +7,10 @@ com 5 asteriscos de base. Por exemplo: n = 5
 ****
 *****
 */
+
+let n = 5;
+let asterisco = ""; // cria a variável que vai armazenar os *
+for (let i = 1; i <= n; i += 1){ // loop para repetir o número de vezes de acordo com o valor de n.
+    asterisco += "*"; // adiciona um * cada vez que o loop passa por aqui
+    console.log(asterisco); // e logo em seguida imprime na tela, então cada rodada ele vai imprimir um a mais.
+}
