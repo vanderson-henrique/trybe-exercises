194d7dfc60bd2aab050ed484a4f70ff538ef4cab
vanderson-henrique <vhbenedito@gmail.com>
2020-09-04 13:38:26 -0300

    Commit final

--- INTRODUCTION/BLOCO_4/4.4/funções-exercício7.js
+++ INTRODUCTION/BLOCO_4/4.4/funções-exercício7.js
@@ -7,3 +7,21 @@ Retorno esperado: true
 verificaFimPalavra("joaofernando", "fernan");
 Retorno esperado: false */
 
+function verificaFimPalavra (word,ending){
+    let controle = true;
+    word = word.split("");
+    ending = ending.split("");
+
+    for (let i = 0; i < ending.length; i += 1){
+            if (word[word.length-ending.length+i] != ending[i]){
+                controle = false;
+            }
+        }
+        
+    return controle;
+
+
+
+}
+console.log(verificaFimPalavra("trybe","be"));
+console.log(verificaFimPalavra("joaofernando","fernan"));
