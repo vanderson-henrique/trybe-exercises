d43773440a8dacda5ff57f42b6c8ddf26175add5
vanderson-henrique <vhbenedito@gmail.com>
2020-09-18 14:22:55 -0300

    Exercicio 6.1

---
+++ INTRODUCTION/BLOCO_6/6.1/script.js
@@ -0,0 +1,133 @@
+let estados = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
+
+for (let chave in estados){
+    let option = document.createElement('option');
+    let select = document.querySelector('#estado');
+    select.appendChild(option);
+    option.textContent = estados[chave];
+    option.value = chave;
+}
+
+function validacoes(event) {
+    event.preventDefault();
+    // verificação data
+    let dataDigitada = document.querySelector('#data-inicio').value;
+    let arrayData = dataDigitada.split('/');
+    let formatoData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+    if (!formatoData.test(dataDigitada)){
+        alert("Digite a data no formato dd/mm/aaaa");
+    }
+    if (arrayData[0] < 0 || arrayData[0] > 31){
+        alert("Insira um dia válido");
+    }
+    if (arrayData[1] < 0 || arrayData[1] > 12){
+        alert("insira um mês válido");
+    }
+    if (arrayData[3] < 0){
+        alert("Insira um ano válido!");
+    }
+
+    // verificação nome
+
+    let nomeDigitado = document.querySelector('#nome').value;
+    if (nomeDigitado === ""){
+        alert('Campo nome preenchimento obrigatório!');
+    }
+
+    let emailDigitado = document.querySelector('#email').value;
+    if (emailDigitado === ""){
+        alert('Campo e-mail preenchimento obrigatório!');
+    }
+
+    let cpfDigitado = document.querySelector('#cpf').value;
+    if (cpfDigitado === ""){
+        alert('Campo CPF preenchimento obrigatório!');
+    }
+    
+    let endereçoDigitado = document.querySelector('#endereço').value;
+    if (endereçoDigitado === ""){
+        alert('Campo endereço preenchimento obrigatório!');
+    }
+
+    let cidadeDigitado = document.querySelector('#cidade').value;
+    if (cidadeDigitado === ""){
+        alert('Campo cidade preenchimento obrigatório!');
+    }
+
+    let resumoDigitado = document.querySelector('#resumo').value;
+    if (resumoDigitado === ""){
+        alert('Campo Resumo do currículo preenchimento obrigatório!');
+    }
+
+    let cargoDigitado = document.querySelector('#cargo').value;
+    if (cargoDigitado === ""){
+        alert('Campo cargo preenchimento obrigatório!');
+    }
+
+    let descCargoDigitado = document.querySelector('#desc-cargo').value;
+    if (descCargoDigitado === ""){
+        alert('Campo Descrição do cargo preenchimento obrigatório!');
+    }
+
+    let estado;
+    let estadosSelecionados = document.querySelectorAll('option');
+
+    if (estadosSelecionados[0].selected == true){
+        alert('Selecione o seu estado!');
+    }
+
+    for (let index = 0; index < 28; index += 1){
+        if (estadosSelecionados[index].selected == true){
+            estado = estadosSelecionados[index].textContent;
+        }
+    }
+
+    let tipoMoradia = document.querySelector('.form-tipo').childNodes;
+    let moradiaselecionada;
+    if (tipoMoradia[3].checked == true){
+         moradiaselecionada = tipoMoradia[3].value;
+    }
+
+    if (tipoMoradia[9].checked == true){
+        moradiaselecionada = tipoMoradia[9].value;
+    }
+    
+    let curriculo = document.createElement('div');
+    let body = document.querySelector('body');
+    body.appendChild(curriculo);
+    curriculo.innerHTML = `Nome: ${nomeDigitado}, E-mail: ${emailDigitado}, CPF: ${cpfDigitado}, Endereço: ${endereçoDigitado}, Cidade: ${cidadeDigitado}, Estado: ${estado}, Tipo de moradia: ${moradiaselecionada}, Resumo do currículo: ${resumoDigitado}, Último cargo: ${cargoDigitado}, Descrição do cargo: ${descCargoDigitado}, Data de início: ${dataDigitada}. `
+
+
+
+}
+
+let botaoEnviar = document.querySelector('#enviar');
+botaoEnviar.addEventListener('click', validacoes);
