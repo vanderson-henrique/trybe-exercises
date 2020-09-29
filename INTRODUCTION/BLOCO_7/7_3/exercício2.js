65177209b87296bb2456588cb4ee942e2f207f9f
vanderson-henrique <vhbenedito@gmail.com>
2020-09-29 20:38:06 -0300

    TDD - Exercício 1

--- INTRODUCTION/BLOCO_7/7_3/exercício2.js
+++ INTRODUCTION/BLOCO_7/7_3/exercício2.js
@@ -18,7 +18,9 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3),[1, 2, 4]);
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3),[1, 2, 3, 4]);
 
 // 3 - Verifique se o array passado por parâmetro não sofreu alterações
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3));
+const myArray = [1, 2, 3, 4];
+myRemove(myArray, 3);
+assert.deepStrictEqual(myArray, [1, 2, 3, 4]);
 
 // 4 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
--- INTRODUCTION/BLOCO_7/7_3/exercício3.js
+++ INTRODUCTION/BLOCO_7/7_3/exercício3.js
@@ -20,6 +20,9 @@ assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3),[1, 2, 4]);
 assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 
 // 3 - Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+const myArray = [1, 2, 3, 4];
+myRemoveWithoutCopy(myArray, 3);
+assert.notDeepStrictEqual(myArray, [1, 2, 3, 4]);
 
 // 4 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
