26d038b46406be257f78569b4dea3718e59eae3b
vanderson-henrique <vhbenedito@gmail.com>
2020-10-07 16:01:28 -0300

    Exercício 3

---
+++ INTRODUCTION/BLOCO_8/8_3/exercício5.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+function containsA() {
+  // escreva seu código aqui
+}
+
+assert.deepEqual(containsA(), 20);
