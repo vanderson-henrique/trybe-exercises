112d41b0336a0753968487838d11d91986380dc8
vanderson-henrique <vhbenedito@gmail.com>
2020-10-07 18:27:07 -0300

    Exercício 5

--- INTRODUCTION/BLOCO_8/8_3/exercício5.js
+++ INTRODUCTION/BLOCO_8/8_3/exercício5.js
@@ -6,10 +6,21 @@ const names = [
   'Abeladerco', 'Adieidy',  'Alarucha',
 ];
 
-// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra 'a' maiúscula ou minúscula.
 
 function containsA() {
   // escreva seu código aqui
+
+  const count = names.reduce((accumulator, word) => {
+    for (let i in word) {
+      if (word[i] === 'a' || word[i] === 'A') {
+        accumulator += 1;
+      }
+    }
+    return accumulator;
+  }, 0);
+  return count;
 }
 
+console.log(containsA());
 assert.deepEqual(containsA(), 20);
