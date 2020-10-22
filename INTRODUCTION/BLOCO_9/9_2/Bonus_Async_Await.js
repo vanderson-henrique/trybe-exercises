357d128bb002d43aec5094d8d6825b14b1f9de20
vanderson-henrique <vhbenedito@gmail.com>
2020-10-22 18:29:30 -0300

    Exercícios Bloco 9 - dia 9.2

--- INTRODUCTION/BLOCO_9/9_2/Bonus_Async_Await.js
+++ INTRODUCTION/BLOCO_9/9_2/Bonus_Async_Await.js
@@ -1,7 +1,6 @@
 // Refatore a Promise para utilizar somente async e await.
 
-const operationsWithNumbers = ()  => {
-    return new Promise((resolve, reject) => {
+async function operationsWithNumbers() {
     const arrayNumber = [];
     for (let i = 0; i < 10; i += 1) {
         arrayNumber.push(Math.floor(Math.random() * 50));
@@ -10,27 +9,34 @@ const operationsWithNumbers = ()  => {
     const sumSquaredNumber = squaredNumber.reduce((sum, number) => sum += number, 0);
     
     if (sumSquaredNumber < 8000) {
-      resolve(sumSquaredNumber);
+      return(sumSquaredNumber);
     }
-    reject('É mais de oito mil');
-});
+    throw new Error('É mais de oito mil');
 };
 
-operationsWithNumbers()
-  .then(number => numberDivisions(number))
-  .then(array => console.log(somaArray(array)))
-  .catch(msg => console.log(`${msg}! Essa promise deve estar quebrada!`));
+async function returnOperations() {
+    try {
+        const numberResponse = await operationsWithNumbers();
+        const numberDivididos = await numberDivisions(numberResponse);
+        const arraySum = await somaArray(numberDivididos);
+        console.log(arraySum);
+    } catch (error) {
+        console.log(`${error}! Essa promise deve estar quebrada!`);
+    }
+}
 
 // Função para dividir o array retornado por 2, 3, 5 e 10
-const numberDivisions = (number) => {
+async function numberDivisions(number) {
     const arrayDivisors = [2, 3, 5, 10];
     const arrayDivisions = arrayDivisors.map(divisor => Math.floor(number / divisor));
     return arrayDivisions;
 };
 
 // Função para somar os números do array acima
-const somaArray = (arrayNumber) => {
+async function somaArray(arrayNumber) {
     return arrayNumber.reduce((sum, number) => {
         return sum += number;
     }, 0);
 };
+
+returnOperations();
