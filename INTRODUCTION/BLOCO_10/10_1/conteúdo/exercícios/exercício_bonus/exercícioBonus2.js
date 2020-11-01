08727eff18b5c5e88fe0e86cad08180b587e3f93
vanderson-henrique <vhbenedito@gmail.com>
2020-11-01 12:13:02 -0300

    Exercicio Bonus dia 10.1

--- INTRODUCTION/BLOCO_10/10_1/conteúdo/exercícios/exercício_bonus/exercícioBonus2.js
+++ INTRODUCTION/BLOCO_10/10_1/conteúdo/exercícios/exercício_bonus/exercícioBonus2.js
@@ -1,11 +1,24 @@
 // setPrice(item: Object, price: Number) => item: Object
 const setPrice = (item, price) => {
-    // TODO: implement
-  };
-  
-  // addToCart(cart: Array, item: Object) => cart: Array
-  const addToCart = (cart, item) => {
-    // TODO: implement
-  };
+  // item é um objeto que contem name e price
+  // price é o novo preço do produto
+  // o objeto item não pode ser modificado;
 
-  module.exports = {setPrice, addToCart};
+  const newItem = { name, price };
+  newItem.name = item.name;
+  newItem.price = price;
+  return newItem;
+};
+
+// addToCart(cart: Array, item: Object) => cart: Array
+const addToCart = (cart, item) => {
+  // cart é um array
+  // item é o item que deverá ser incluído em um array diferente
+  // deve-se criar um novo array
+
+  const newArray = [];
+  newArray.push(item);
+  return newArray;
+};
+
+module.exports = { setPrice, addToCart };
