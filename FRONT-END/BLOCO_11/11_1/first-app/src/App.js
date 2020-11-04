522315064243e4b019a219cb607694d38fc53939
vanderson-henrique <vhbenedito@gmail.com>
2020-11-04 14:37:02 -0300

    Bloco 11 - Hello World em React

---
+++ FRONT-END/BLOCO_11/11_1/first-app/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Component from './Component';
+
+function App() {
+  return <Component />
+}
+
+export default App;
---
+++ FRONT-END/BLOCO_11/11_1/hello-world-react/src/HelloWorld.js
@@ -0,0 +1,10 @@
+// é possível importar todo o objeto react ou apenas uma propriedade já desestruturada
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+    render() {
+        return <span className='helloWorld'>Hello World!</span>
+    }
+}
+
+export default HelloWorld;
