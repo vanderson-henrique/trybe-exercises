545be08343cb1a4d9bbc6140aaa0ff4b849e1655
vanderson-henrique <vhbenedito@gmail.com>
2021-02-22 18:19:34 -0300

    Exercício sobre HAVING

--- BACK-END/BLOCO_21/CONTEÚDO_BLOCO_21_DIA_01.sql
+++ BACK-END/BLOCO_21/CONTEÚDO_BLOCO_21_DIA_01.sql
@@ -220,6 +220,28 @@ SELECT district,
 	FROM sakila.address
     GROUP BY district
     ORDER BY `Número de endereços` DESC;
+    
+-- USANDO O HAVING PARA FILTRAR DADOS AGRUPADOS
+
+/* Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50.
+Além disso, dê um alias (apelido) à coluna gerada por AVG(length) , de forma que deixe a query mais legível.
+Finalize ordenando os resultados de forma decrescente.*/
+
+    SELECT rating, AVG(length) AS media_length
+    FROM sakila.film
+    GROUP BY rating
+    HAVING media_length BETWEEN 115.0 AND 121.50
+    ORDER BY media_length DESC;
+
+/* Usando a query a seguir, exiba apenas os valores de total de substituição de custo que estão acima de $3950.50.
+Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível.
+Finalize ordenando os resultados de forma crescente. */
+
+    SELECT rating, SUM(replacement_cost) AS soma_custo_substituicao
+    FROM sakila.film
+    GROUP by rating
+    HAVING soma_custo_substituicao > 3950.50
+    ORDER BY soma_custo_substituicao ASC;
 
 
 
