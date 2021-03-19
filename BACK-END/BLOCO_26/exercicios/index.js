b2f1df08156f2f0e2bcc202b2f6c257d29c9e21b
vanderson-henrique <vhbenedito@gmail.com>
2021-03-19 10:08:59 -0300

    Bloco 26 - dia 01 - Exercício 2

--- BACK-END/BLOCO_26/exercicios/index.js
+++ BACK-END/BLOCO_26/exercicios/index.js
@@ -10,3 +10,23 @@ const altura = questionFloat('Digite a sua altura em metros: ');
 const IMC = calculoIMC(peso, altura);
 
 console.log(`O seu IMC é: ${ IMC }`);
+console.log('Sua situação é: ');
+
+   if( IMC < 18.5 ){
+    console.log('Abaixo do peso (magreza).');
+   }
+  else if( IMC >= 18.5 && IMC <= 24.9 ) {
+    console.log('Peso normal.');
+  }
+  else if( IMC >= 25 && IMC <= 29.9 ) {
+    console.log('Acima do peso (sobrepeso)');
+  }
+  else if( IMC >= 30 && IMC <= 34.9 ) {
+    console.log('Obesidade grau I');
+  }
+  else if( IMC >= 35 && IMC <= 39.9 ) {
+    console.log('Obesidade grau II');
+  }
+  else {
+    console.log('Obesidade grau III e IV');
+  }
