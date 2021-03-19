24f056b3c6d9e9a05a36d7c3749b6574a58200d4
vanderson-henrique <vhbenedito@gmail.com>
2021-03-19 11:03:14 -0300

    Exercício 3 - bloco 26 dia 01

--- BACK-END/BLOCO_26/exercicios/index.js
+++ BACK-END/BLOCO_26/exercicios/index.js
@@ -1,20 +1,33 @@
-const { questionFloat } = require('readline-sync');
+const inquirer = require('inquirer');
 
 function calculoIMC(peso, altura) {
   return (peso / Math.pow(altura, 2)).toFixed(2);
 };
 
-const peso = questionFloat('Digite o seu peso em KG: ');
-const altura = questionFloat('Digite a sua altura em metros: ');
+// const peso = questionFloat('Digite o seu peso em KG: ');
+// const altura = questionFloat('Digite a sua altura em metros: ');
+
+function validateFloat(input) {
+  return !isNaN(parseFloat(input)) || 'Por favor, digite um número válido';
+}
+
+async function changeInfo() {
+  const answers = await inquirer.prompt([
+    { name: 'peso', type: 'input', message: 'Digite o seu peso em KG: ', validate: validateFloat },
+    { name: 'altura', type: 'input', message: 'Digite a sua altura em metros: ', validate: validateFloat }
+  ]);
+
+const peso = parseFloat(answers.peso);
+const altura = parseFloat(answers.altura);
 
 const IMC = calculoIMC(peso, altura);
 
 console.log(`O seu IMC é: ${ IMC }`);
 console.log('Sua situação é: ');
 
-   if( IMC < 18.5 ){
-    console.log('Abaixo do peso (magreza).');
-   }
+  if( IMC < 18.5 ){
+  console.log('Abaixo do peso (magreza).');
+  }
   else if( IMC >= 18.5 && IMC <= 24.9 ) {
     console.log('Peso normal.');
   }
@@ -30,3 +43,6 @@ console.log('Sua situação é: ');
   else {
     console.log('Obesidade grau III e IV');
   }
+}
+
+changeInfo();
