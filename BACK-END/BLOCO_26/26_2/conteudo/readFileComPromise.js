dc2888e3495183eacaf93710f9f8659b2a26641f
vanderson-henrique <vhbenedito@gmail.com>
2021-03-22 15:51:36 -0300

    Conteúdo e exercicios bloco 26 dia 02

---
+++ BACK-END/BLOCO_26/26_2/conteudo/readFileComPromise.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+function readFileComPromise(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, (err, content) => {
+      if (err) return reject(err);
+      resolve(content);
+    });
+  });
+}
---
+++ BACK-END/BLOCO_26/26_2/conteudo/writeFile.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+fs.writeFile('./meu-arquivo.txt', 'Meu textão', (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Arquivo salvo');
+});
